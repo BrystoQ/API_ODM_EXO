b5ef3ec98ebe302d679f32d9788e4d336d1ec702
Quentin Aubert <9610898-quentin.aubert@users.noreply.gitlab.com>
2022-11-10 10:59:15 +0100

    feat(api): Add API learn in class

---
+++ back/src/api/controllers/userController.js
@@ -0,0 +1,77 @@
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { updateOne } = require("../models/userModel");
+
+exports.userRegister = (req, res) => {
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
+      user
+        .save()
+        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
+
+exports.loginRegister = (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        res
+          .status(401)
+          .json({ message: "Paire Identifiant/mot de passe incorrecte" });
+      } else {
+        bcrypt
+          .compare(req.body.password, user.password)
+          .then((valid) => {
+            if (!valid) {
+              res
+                .status(401)
+                .json({ message: "Paire Identifiant/mot de passe incorrecte" });
+            } else {
+              res.status(200);
+              let userData = {
+                id: user._id,
+                email: user.email,
+                role: "admin",
+              };
+              jwt.sign(
+                userData,
+                process.env.JWT_KEY,
+                { expiresIn: "30 days" },
+                (error, token) => {
+                  if (error) {
+                    res.status(500);
+                    console.log(error);
+                    res.json({ message: "Impossible de générer le token" });
+                  } else {
+                    res.status(200);
+                    res.json({ token });
+                  }
+                }
+              );
+            }
+          })
+          .catch((error) => res.status(500).json({ error }));
+      }
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
+
+exports.roleUser = (req, res) => {
+  User.updateOne({ email: req.body.email })
+    .then()
+    .catch((error) => res.status(401).json({ error }));
+};
+
+// Used for Debug
+exports.getUsers = (req, res) =>
+  User.find()
+    .then((things) => res.status(200).json(things))
+    .catch((error) => res.status(400).json({ error }));
