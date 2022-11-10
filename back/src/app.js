8a84a5cc416941915ce80504b5b8a7e5f026f6a2
Quentin Aubert <9610898-quentin.aubert@users.noreply.gitlab.com>
2022-11-10 16:22:40 +0100

    feat(api): Add cord

--- back/src/app.js
+++ back/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 
 const hostname = "0.0.0.0";
 const port = 3000;
@@ -8,6 +9,8 @@ const server = express();
 const mongoose = require("mongoose");
 // mongoose.connect("mongodb://localhost:27017/apinode"); // Whithout Docker
 mongoose.connect("mongodb://mongo/apinode");
+
+server.use(cors()); //Cors config
 
 server.use(express.urlencoded());
 server.use(express.json());
